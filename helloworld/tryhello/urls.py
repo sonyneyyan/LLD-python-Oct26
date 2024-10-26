10f7f3cca4092ecc85ad47607e874702da174dab
sony neyyan <neyysony@in.ibm.com>
2024-10-26 15:43:24 +0530

    passing name in the parameter

--- helloworld/tryhello/urls.py
+++ helloworld/tryhello/urls.py
@@ -3,4 +3,4 @@ from django.urls import path
 from tryhello import views
 
 
-urlpatterns = [path('hello/',views.helloworld,name='hello')]
+urlpatterns = [path('hello/<str:name>',views.helloworld,name='hello')]
--- helloworld/tryhello/views.py
+++ helloworld/tryhello/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def helloworld(request):
-    return HttpResponse("Hello World!")
+def helloworld(request,name):
+    return HttpResponse("Hello " + name)
