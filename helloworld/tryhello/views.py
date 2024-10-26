931d39d3202425b8a14adaae4d216fb661479089
sony neyyan <neyysony@in.ibm.com>
2024-10-26 16:41:29 +0530

    get a student age by name from the db

--- helloworld/tryhello/views.py
+++ helloworld/tryhello/views.py
@@ -6,6 +6,8 @@ from tryhello.models import Student
 
 # Create your views here.
 def helloworld(request,name):
-    s1 = Student(name=name,age=10,dob=datetime.date.today())
-    s1.save() # until and unless we save it it will not reflected in db.
-    return HttpResponse("Hello " + name)
+    #s1 = Student(name=name,age=10,dob=datetime.date.today())
+    #s1.save() # until and unless we save it it will not reflected in db.
+    # getting the data from db for a particular student by name
+    student = Student.objects.get(name=name)
+    return HttpResponse("Hello your age is " + str(student.age))
