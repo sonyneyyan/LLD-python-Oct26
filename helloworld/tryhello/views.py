18391e89cdf52517a7bf8df126bd58c4932fe5c3
sony neyyan <neyysony@in.ibm.com>
2024-10-26 16:38:37 +0530

    create a entry in the db by providing the classs details

--- helloworld/tryhello/views.py
+++ helloworld/tryhello/views.py
@@ -1,6 +1,11 @@
+import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from tryhello.models import Student
+
 # Create your views here.
 def helloworld(request,name):
+    s1 = Student(name=name,age=10,dob=datetime.date.today())
+    s1.save() # until and unless we save it it will not reflected in db.
     return HttpResponse("Hello " + name)
