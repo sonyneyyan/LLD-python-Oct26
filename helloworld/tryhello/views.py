d625c2622808694dd3d3fc09bee629a1ec5ef2b0
sony neyyan <neyysony@in.ibm.com>
2024-10-26 16:45:42 +0530

    updating the age of the student from the views.py

--- helloworld/tryhello/views.py
+++ helloworld/tryhello/views.py
@@ -10,4 +10,6 @@ def helloworld(request,name):
     #s1.save() # until and unless we save it it will not reflected in db.
     # getting the data from db for a particular student by name
     student = Student.objects.get(name=name)
+    student.age = 40
+    student.save()
     return HttpResponse("Hello your age is " + str(student.age))
