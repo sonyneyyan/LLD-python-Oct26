4cb54529295e55724e9abaf47447c59f397b854b
sony neyyan <neyysony@in.ibm.com>
2024-10-27 11:05:38 +0530

    Add new application library for books and authors

--- helloworld/library/apps.py
+++ helloworld/library/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LibConfig(AppConfig):
+class LibraryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lib'
+    name = 'library'
