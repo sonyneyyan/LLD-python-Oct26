9e0a089dff8439624ab88b27c4eb0c5b370c1427
sony neyyan <neyysony@in.ibm.com>
2024-10-27 10:14:20 +0530

    Add enrollment and course to admin pane

--- helloworld/tryhello/admin.py
+++ helloworld/tryhello/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from tryhello.models import Student
+from tryhello.models import Course, Enrollment, Student
 
 # Register your models here.
 
-admin.site.register(Student)
+admin.site.register(Student) # This is to register the student class to the admin panel 
+admin.site.register(Course)
+admin.site.register(Enrollment)
