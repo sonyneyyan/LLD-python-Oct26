e8c3793255a14d72248292a061eaa846cb2c855a
sony neyyan <neyysony@in.ibm.com>
2024-10-27 10:32:34 +0530

    in admin panel conf change the editable and filter fields to tuple

--- helloworld/tryhello/admin.py
+++ helloworld/tryhello/admin.py
@@ -4,15 +4,15 @@ from tryhello.models import Course, Enrollment, Student
 
 # Register your models here.
 
-admin.site.register(Student) # This is to register the student class to the admin panel 
+#admin.site.register(Student) # This is to register the student class to the admin panel 
 admin.site.register(Course)
 admin.site.register(Enrollment)
 
-class studentAdmin(admin.ModelAdmin):
-    list_filter = ('name')
+class StudentAdmin(admin.ModelAdmin):
+    list_filter = (('name'),)
     list_display = ('name','age')
-    list_editable = ('name')
+    list_editable = (('age'),)
 
 # studentAdmin is related to Student table , this is done by the below code.
 
-admin.site.register(Student,studentAdmin)
+admin.site.register(Student,StudentAdmin)
