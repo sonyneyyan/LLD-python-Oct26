8f68361273ffa7e6ec7b428f128deaa2661257d2
sony neyyan <neyysony@in.ibm.com>
2024-10-27 10:27:59 +0530

    create custom admin panel

--- helloworld/tryhello/admin.py
+++ helloworld/tryhello/admin.py
@@ -7,3 +7,12 @@ from tryhello.models import Course, Enrollment, Student
 admin.site.register(Student) # This is to register the student class to the admin panel 
 admin.site.register(Course)
 admin.site.register(Enrollment)
+
+class studentAdmin(admin.ModelAdmin):
+    list_filter = ('name')
+    list_display = ('name','age')
+    list_editable = ('name')
+
+# studentAdmin is related to Student table , this is done by the below code.
+
+admin.site.register(Student,studentAdmin)
